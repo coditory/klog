4aed5324b9385d63840dd95222ff15a985292a11
ogesaku <ogesaku@gmail.com>
2023-12-29 22:24:01 +0100

    Add github actions

--- build-logic/src/main/kotlin/build.kotlin.gradle.kts
+++ build-logic/src/main/kotlin/build.kotlin.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("build.java")
     kotlin("jvm")
     id("org.jlleitschuh.gradle.ktlint")
+    id("org.jetbrains.dokka")
 }
 
 val libs = extensions.getByType(org.gradle.accessors.dm.LibrariesForLibs::class)
