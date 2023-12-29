11c6f489502ebd8119312f4a66b38902dd75f1d8
ogesaku <ogesaku@gmail.com>
2023-12-29 22:24:01 +0100

    Add github actions

--- klog/src/test/kotlin/com/coditory/klog/KlogSpec.kt
+++ klog/src/test/kotlin/com/coditory/klog/KlogSpec.kt
@@ -9,27 +9,31 @@ import io.kotest.matchers.shouldBe
 class KlogSpec : FunSpec({
     val clock = UpdatableFixedClock()
     val publisher = InMemoryPublisher()
-    val klog = Klog(klogConfig {
-        clock(clock)
-        stream {
-            blockingPublisher(publisher)
-        }
-    })
+    val klog =
+        Klog(
+            klogConfig {
+                clock(clock)
+                stream {
+                    blockingPublisher(publisher)
+                }
+            },
+        )
     val logger = klog.logger("com.coditory.Logger")
 
     test("should emit a basic log event") {
         logger.info { "Hello" }
         publisher.getLogs().size shouldBe 1
-        publisher.getLastLog() shouldBe LogEvent(
-            priority = LogPriority.STANDARD,
-            timestamp = clock.zonedDateTime(),
-            logger = "com.coditory.Logger",
-            level = Level.INFO,
-            thread = Thread.currentThread().name,
-            message = "Hello",
-            throwable = null,
-            context = emptyMap(),
-            items = emptyMap(),
-        )
+        publisher.getLastLog() shouldBe
+            LogEvent(
+                priority = LogPriority.STANDARD,
+                timestamp = clock.zonedDateTime(),
+                logger = "com.coditory.Logger",
+                level = Level.INFO,
+                thread = Thread.currentThread().name,
+                message = "Hello",
+                throwable = null,
+                context = emptyMap(),
+                items = emptyMap(),
+            )
     }
 })
